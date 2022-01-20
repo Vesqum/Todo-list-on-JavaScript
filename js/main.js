3d5c46e6c8231b0e251cfcd6e21665a881fb3379
Vesqum <marcin.pelszyk30@gmail.com>
2022-01-20 18:57:44 +0100

    added functions to add tasks

--- js/main.js
+++ js/main.js
@@ -14,10 +14,56 @@ const prepareDOMElements = () => {
     addBtn = document.querySelector('.btn-add')
     ulList = document.querySelector('.todolist ul')
 
+    
+
 }
 
 const prepareDOMEvent = () => {
-
+    addBtn.addEventListener('click', addNewTodo)
 }
+
+const addNewTodo = () => {
+
+    if (todoInput.value !== '') {
+        newTodo = document.createElement('li')
+        newTodo.textContent = todoInput.value;
+        createToolsArea(newTodo)
+
+        ulList.append(newTodo)
+
+        errorInfo.textContent = ''
+        todoInput.textContent = ''
+    } else {
+        errorInfo.textContent = 'Wpisz treść zadania...'
+    }
+}
+
+const createToolsArea = (newTodo) => {
+
+    if (todoInput.value !== '') {
+        const toolsPanel = document.createElement('div')
+        toolsPanel.classList.add('tools')
+        newTodo.append(toolsPanel)
+
+
+        const complateBtn = document.createElement('button')
+        complateBtn.classList.add('complate')
+
+        complateBtn.innerHTML = '<i class="fas fa-check"></i>'
+        const editBtn = document.createElement('button')
+        editBtn.classList.add('edit')
+        editBtn.textContent = 'EDIT'
+
+        const deleteBtn = document.createElement('button')
+        deleteBtn.classList.add('delete')
+        deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+        toolsPanel.append(complateBtn, editBtn, deleteBtn)
+        
+    }
+}
+
+
+
 
 document.addEventListener('DOMContentLoaded', main)
